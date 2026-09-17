f7a69a6ccdca4d787296c6cbd9004887fb7f4ab0
Noe Marley Ginting <72056796+ChunMargin@users.noreply.github.com>
2026-09-17 19:32:48 +0700

    Gabungan dari semua branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,4 @@
 #include <Arduino.h>
-<<<<<<< Updated upstream
-#include <DHT.h>
-
-#define DHTPIN 4      // GPIO4, pin aman untuk DHT11 pada ESP32-C3
-#define DHTTYPE DHT11 // sensor DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
-
-void setup() {
-  Serial.begin(115200);
-  delay(1000); // tunggu serial USB siap
-  Serial.println("Monitoring sensor DHT11...");
-  dht.begin();
-}
-
-void loop() {
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Gagal membaca sensor DHT11!");
-  } else {
-    Serial.print("Kelembapan: ");
-    Serial.print(humidity);
-    Serial.print("%\t");
-    Serial.print("Suhu: ");
-    Serial.print(temperature);
-    Serial.println("°C");
-  }
-
-  delay(1000);
-=======
 #include <Wire.h>
 #include <BH1750.h>
 #include <DHT.h>
@@ -111,5 +79,4 @@ void loop() {
   }
 
   delay(100);
->>>>>>> Stashed changes
 }
